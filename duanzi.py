eb896940ba34617fd2286b48b3cf92111b88667a
iotyib <iotyib@163.com>
2018-08-24 22:45:02 +0800

    内涵段子爬虫

--- duanzi.py
+++ duanzi.py
@@ -1,6 +1,7 @@
 #-*-coding:utf-8-*-
 
 import urllib2
+import re
 
 class Spider(object):
     '''
@@ -20,8 +21,30 @@ class Spider(object):
         req = urllib2.Request(url,headers=headers)
         response = urllib2.urlopen(req)
         html = response.read()
-        print html
+        # print html
+        pattern = re.compile(r'<div.*?class="desc">(.*?)</div>',re.S)
+        lists = re.findall(pattern,html)
+        return lists
+
+    def printOnePage(self,lists,page):
+        '''
+        @brief 处理得到的列表
+        :param lists:得到的数据
+        :param page:页数
+        :return:
+        '''
+        print '-*-*-*-*-第%d页爬取完毕*-*-*-*-*-'%page
+        for item in lists:
+            self.writeToFile(item)
+
+    def writeToFile(self,text):
+        with open('duanzi.txt','a+') as f:
+            f.write('-*-*-*-*-*-*-*-*-*-*-*-*-*---*-**-'+'\n')
+            f.write(text + '\n')
+            f.close()
+
 
 if __name__ == '__main__':
     myspider = Spider()
-    myspider.load(2)
+    lists = myspider.load(2)
+    myspider.printOnePage(lists,2)
